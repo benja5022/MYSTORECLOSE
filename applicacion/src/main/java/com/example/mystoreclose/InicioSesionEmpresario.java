f9b4ac6210470c66c6fc04f83da0690166674a9c
benja5022 <benjaui5022@gmail.com>
2022-09-19 23:56:37 -0400

    Colo Colo

---
+++ applicacion/src/main/java/com/example/mystoreclose/InicioSesionEmpresario.java
@@ -0,0 +1,14 @@
+package com.example.mystoreclose;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioSesionEmpresario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion_empresario);
+    }
+}
---
+++ applicacion/src/main/java/com/example/mystoreclose/RegistroMiniMarket.java
@@ -0,0 +1,14 @@
+package com.example.mystoreclose;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroMiniMarket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_mini_market);
+    }
+}
